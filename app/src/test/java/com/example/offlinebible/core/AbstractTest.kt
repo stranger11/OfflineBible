c0ae3bcc63c1538bd1168be96d58479fcbeb6a08
yan <yan.p@fideware.by>
2022-09-13 17:58:46 +0300

    add abstract and abstract test

---
+++ app/src/test/java/com/example/offlinebible/core/AbstractTest.kt
@@ -0,0 +1,72 @@
+package com.example.offlinebible.core
+
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+
+class AbstractTest {
+
+    @Test
+    fun test_success() {
+        val dataObject = TestDataObject.Success("a", "b")
+        val domainObject = dataObject.map(DataToDomainMapper.Base())
+        assertTrue(domainObject is DomainObject.Success)
+    }
+
+    private sealed class TestDataObject : Abstract.Object<DomainObject, DataToDomainMapper>() {
+        abstract override fun map(mapper: DataToDomainMapper): DomainObject
+
+        class Success(
+            private val textOne: String,
+            private val textTwo: String
+        ) : TestDataObject() {
+            override fun map(mapper: DataToDomainMapper): DomainObject {
+                return mapper.map(textOne, textTwo)
+            }
+        }
+
+        class Fail(private val exception: Exception) : TestDataObject() {
+            override fun map(mapper: DataToDomainMapper): DomainObject {
+                return mapper.map(exception)
+            }
+        }
+    }
+
+    private interface DataToDomainMapper : Abstract.Mapper {
+
+        fun map(textOne: String, textTwo: String): DomainObject
+
+        fun map(exception: Exception): DomainObject
+
+        class Base: DataToDomainMapper {
+            override fun map(textOne: String, textTwo: String): DomainObject {
+                return DomainObject.Success("$textOne $textTwo")
+            }
+
+            override fun map(exception: Exception): DomainObject {
+                return DomainObject.Fail()
+            }
+
+        }
+    }
+
+    private sealed class DomainObject : Abstract.Object<UiObject, DomainToUiMapper>() {
+        class Success(private val textCombined: String) : DomainObject() {
+            override fun map(mapper: DomainToUiMapper): UiObject {
+                TODO()
+            }
+
+        }
+
+        class Fail() : DomainObject() {
+            override fun map(mapper: DomainToUiMapper): UiObject {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
+
+    private interface DomainToUiMapper : Abstract.Mapper
+
+    private sealed class UiObject
+}
